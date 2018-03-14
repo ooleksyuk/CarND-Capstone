223882ae799ca1017c7429162819d577848e4839
Isharaka Gunasinghe <isharaka.gunasinghe@fovio.com>
2018-03-14 21:36:45 +1100

    used kd tree to find closest waypoint

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -42,6 +42,7 @@ class WaypointUpdater(object):
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.waypoint_tree = None
         self.tf_listener = tf.TransformListener()
         self.red_light_waypoint = None
         self.next_waypoint = -1
@@ -113,6 +114,8 @@ class WaypointUpdater(object):
             self.waypoints_stamped.waypoints[i].pose.header.frame_id = self.waypoints_stamped.header.frame_id
             self.set_waypoint_velocity(self.waypoints_stamped.waypoints, i, 0.0)
 
+        self.waypoint_tree = kdtree([(waypoint.pose.pose.position.x, waypoint.pose.pose.position.y) for waypoint in self.waypoints_stamped.waypoints], 2)
+
     def traffic_cb(self, msg):
         self.red_light_waypoint = msg.data
 
@@ -158,21 +161,10 @@ class WaypointUpdater(object):
             int: index of the closest waypoint in self.waypoints
 
         """
-        #TODO This function is currently O(n). It can be improved to O(log n)
-        if self.waypoints_stamped == None:
+        if self.waypoint_tree == None:
             return None
 
-        dist_min = sys.maxsize
-        wp_min = None
-
-        for wp in range(len(self.waypoints_stamped.waypoints)):
-            dist = self.distance2(pose, self.waypoints_stamped.waypoints[wp].pose.pose)
-
-            if dist < dist_min:
-                dist_min = dist
-                wp_min = wp
-
-        return wp_min
+        return self.waypoint_tree.closest((pose.position.x, pose.position.y))[0]
 
     def transform_to_car_frame(self, pose_stamped):
         """Transform the given pose to car co-ordinate frame
@@ -235,6 +227,68 @@ class WaypointUpdater(object):
         """
         # The +1 is to make the speed at 0th waypoin non-zero. Waypoint follower seem to want this.
         return self.current_speed + (waypoint + 1) * self.trajectory_acceleration
+
+class kdtree:
+    def __init__(self, points, dimensions):
+        self.dimensions = dimensions
+        self.tree = self._build([(i, p) for i,p in enumerate(points)])
+        
+    def closest(self, point):
+        return self._closest(self.tree, point)
+        
+    def _build(self,points, depth=0):
+        n = len(points)
+
+        if n<= 0:
+            return None
+
+        dimension = depth % self.dimensions
+        sorted_points = sorted(points, key=lambda point:point[1][dimension])
+
+        mid = n//2
+
+        return {
+            'mid':sorted_points[mid],
+            'left':self._build(sorted_points[:mid], depth+1),
+            'right':self._build(sorted_points[mid+1:], depth+1)
+        }
+    
+    def _distance(self, point, node):
+        if point == None or node == None:
+            return float('Inf')
+        
+        d = 0
+        for p,n in zip(list(point),list(node[1])):
+            d += (p - n)**2
+        
+        return d
+    
+    def _closest(self, root, point, depth=0):
+        if root is None:
+            return None
+
+        dimension = depth % self.dimensions   
+        mid_point = root['mid']
+        
+        next_branch     = root['left'] if point[dimension] < mid_point[1][dimension] else root['right']
+        opposite_branch = root['right'] if point[dimension] < mid_point[1][dimension] else root['left']
+
+        best_in_next_branch = self._closest(next_branch, point, depth+1)
+
+        distance_to_mid_point = self._distance(point, mid_point)
+        distance_to_next_branch = self._distance(point, best_in_next_branch)
+
+        best = best_in_next_branch if distance_to_next_branch < distance_to_mid_point else mid_point
+        best_distance = min(distance_to_next_branch, distance_to_mid_point)
+
+        if best_distance > abs(point[dimension] - mid_point[1][dimension]):
+            best_in_opposite_branch = self._closest(opposite_branch, point, depth+1)
+            distance_to_opposite_branch = self._distance(point, best_in_opposite_branch)
+            best = best_in_opposite_branch if distance_to_opposite_branch < best_distance else best
+
+        return best
+
+
 
 if __name__ == '__main__':
     try:
